89f16c7d9a43bf172e63e5c633f82ca55c928f67
Denis Alginin <alginindk@gmail.com>
2025-02-23 22:16:39 +0500

    feat: реализован CRUD сервис для задач с логированием через аспекты
    - Реализованы эндпоинты для создания, получения, обновления и удаления задач.
    - Добавлен функционал получения списка всех задач.
    - Внедрены аспекты с логированием:
      - @Before: логирование до выполнения методов.
      - @AfterThrowing: логирование ошибок.
      - @AfterReturning: логирование успешных завершений методов.
      - @Around: замер времени выполнения методов.

---
+++ src/main/java/task_management/handler/TaskGlobalExeptionHandler.java
@@ -0,0 +1,29 @@
+package task_management.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class TaskGlobalExeptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException.class)
+    public Map<String, String> handleRuntimeExceptions(RuntimeException ex) {
+        return Map.of("error", "Произошла непредвиденная ошибка: " + ex.getMessage());
+    }
+}
